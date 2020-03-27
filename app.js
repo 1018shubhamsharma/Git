a7b7358ac8716143fdec8984ffeef1c1a4ad2fa1
Shubham Sharma <1018shubhamsharma@gmail.com>
2020-03-27 15:27:48 +0530

    because i include all the packages together and there is some problem is occuring.Therefore i am going back and from there i include packages one by one and check that the are properly include.

--- app.js
+++ app.js
@@ -1,9 +1,1 @@
 console.log("Hello from the server file");
-
-const express = require("express"),
-        app   = express(),
-    bodyParser = require("body-parser"),
-    passport  = require("passport"),
-    passportLocal = require("passport-local"),
-    passportLocalMongoose = require("passport-local-mongoose"),
-    mongoose      = require("mongoose");
